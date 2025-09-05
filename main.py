53e77eaaa7bedb18dcc199b5618a86b28df2b7d3
Ifeanyi-design <ifeanyiagada123@gmail.com>
2025-09-05 22:59:07 +0100

    added my secrets to the environment variables

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import requests
 import os
 
 
-API_KEY = "60569c7b45eb595bde23716796fc6612"
+API_KEY = os.environ.get("API_KEY")
 url = "https://api.themoviedb.org/3/search/movie"
 detail_url = "https://api.themoviedb.org/3/movie"
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie-collection.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///movie-collection.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
